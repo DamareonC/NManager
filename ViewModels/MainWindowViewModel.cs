e9f00e54d1732f83341f29308934eac85562367c
DamareonC <markeec2002@gmail.com>
2025-08-07 12:25:51 -0500

    Converted project to MVVM

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,167 @@
+using Avalonia.Input;
+using MsBox.Avalonia;
+using MsBox.Avalonia.Base;
+using MsBox.Avalonia.Enums;
+
+using System;
+using System.Diagnostics;
+using System.IO;
+
+using NManager.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
+
 namespace NManager.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    
+    private readonly MainModel model = new();
+    [ObservableProperty]
+    private bool _BackButtonIsEnabled;
+    [ObservableProperty]
+    private bool _ForwardButtonIsEnabled;
+    [ObservableProperty]
+    private int _ContentListSelectedIndex;
+    [ObservableProperty]
+    private object? _ContentListSelectedItem;
+    [ObservableProperty]
+    private string? _PathTextFieldText;
+    [ObservableProperty]
+    private string[]? _ContentListItemsSource;
+
+    public MainWindowViewModel()
+    {
+        LoadContent(model.CurrentPath);
+    }
+
+    public void OnBackButtonClick()
+    {
+        if (model.PreviousPaths.Count > 0)
+        {
+            model.NextPaths.Push(model.CurrentPath);
+
+            model.CurrentPath = model.PreviousPaths.Pop();
+            PathTextFieldText = model.CurrentPath;
+
+            LoadContent(model.CurrentPath);
+        }
+
+        BackButtonIsEnabled = model.PreviousPaths.Count > 0;
+        ForwardButtonIsEnabled = model.NextPaths.Count > 0;
+    }
+
+    public void OnForwardButtonClick()
+    {
+        if (model.NextPaths.Count > 0)
+        {
+            model.PreviousPaths.Push(model.CurrentPath);
+
+            model.CurrentPath = model.NextPaths.Pop();
+            PathTextFieldText = model.CurrentPath;
+
+            LoadContent(model.CurrentPath);
+        }
+
+        BackButtonIsEnabled = model.PreviousPaths.Count > 0;
+        ForwardButtonIsEnabled = model.NextPaths.Count > 0;
+    }
+
+    public void OnReloadButtonClick()
+    {
+        LoadContent(model.CurrentPath);
+    }
+
+    public void OpenFolderOrLaunchFile()
+    {
+        string slash = OperatingSystem.IsWindows() ? "\\" : "/";
+
+        if (ContentListSelectedIndex < model.FilesStartIndex)
+        {
+            string newCurrentPath = model.CurrentPath + slash + ContentListSelectedItem;
+
+            if (LoadContent(newCurrentPath))
+            {
+                model.PreviousPaths.Push(model.CurrentPath);
+
+                if (model.NextPaths.Count != 0)
+                {
+                    model.NextPaths.Clear();
+                }
+
+                model.CurrentPath = newCurrentPath;
+                PathTextFieldText = model.CurrentPath;
+                BackButtonIsEnabled = model.PreviousPaths.Count > 0;
+                ForwardButtonIsEnabled = model.NextPaths.Count > 0;
+            }
+        }
+        else
+        {
+            Process process = new();
+            ProcessStartInfo processStartInfo = new()
+            {
+                FileName = model.CurrentPath + slash + ContentListSelectedItem,
+                UseShellExecute = true
+            };
+            process.StartInfo = processStartInfo;
+
+            process.Start();
+        }
+    }
+
+    public void LoadPathFromPathTextField(KeyEventArgs args)
+    {
+        if (args.Key == Key.Enter && PathTextFieldText != null && PathTextFieldText.Length > 0 && LoadContent(PathTextFieldText))
+        {
+            model.PreviousPaths.Push(model.CurrentPath);
+
+            if (model.NextPaths.Count != 0)
+            {
+                model.NextPaths.Clear();
+            }
+
+            model.CurrentPath = PathTextFieldText;
+        }
+
+        BackButtonIsEnabled = model.PreviousPaths.Count > 0;
+        ForwardButtonIsEnabled = model.NextPaths.Count > 0;
+    }
+
+    private bool LoadContent(string currentPath)
+    {
+        try
+        {
+            DirectoryInfo directory = new(currentPath);
+            DirectoryInfo[] subdirectories = directory.GetDirectories();
+            FileInfo[] files = directory.GetFiles();
+            string[] content = new string[subdirectories.Length + files.Length];
+
+            for (int i = 0; i < subdirectories.Length; i++)
+            {
+                content[i] = subdirectories[i].Name;
+            }
+
+            for (int i = subdirectories.Length; i < subdirectories.Length + files.Length; i++)
+            {
+                content[i] = files[i - subdirectories.Length].Name;
+            }
+
+            ContentListItemsSource = content;
+            model.FilesStartIndex = subdirectories.Length;
+
+            return true;
+        }
+        catch (DirectoryNotFoundException)
+        {
+            IMsBox<ButtonResult> messageBox = MessageBoxManager.GetMessageBoxStandard("Could Not Find Folder", "The selected folder could not be found.", ButtonEnum.Ok, MsBox.Avalonia.Enums.Icon.Error);
+            messageBox.ShowAsync();
+
+            return false;
+        }
+        catch (UnauthorizedAccessException)
+        {
+            IMsBox<ButtonResult> messageBox = MessageBoxManager.GetMessageBoxStandard("Permission Denied", "You do not have permission to open this folder.", ButtonEnum.Ok, MsBox.Avalonia.Enums.Icon.Error);
+            messageBox.ShowAsync();
+
+            return false;
+        }
+    }
 }
--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,69 +1,41 @@
 using Avalonia.Controls;
 using Avalonia.Input;
 using Avalonia.Interactivity;
-using MsBox.Avalonia;
-using MsBox.Avalonia.Base;
-using MsBox.Avalonia.Enums;
 
-using System;
-using System.Diagnostics;
-using System.IO;
-
-using NManager.Models;
+using NManager.ViewModels;
 
 namespace NManager.Views;
 
 public partial class MainWindow : Window
 {
-    private readonly MainModel model = new();
-
     public MainWindow()
     {
         InitializeComponent();
-        DisplayContent(model.CurrentPath);
-
-        pathTextField.Text = model.CurrentPath;
-        backButton.IsEnabled = model.PreviousPaths.Count > 0;
-        forwardButton.IsEnabled = model.NextPaths.Count > 0;
     }
 
-    private bool DisplayContent(string currentPath)
+    private void MainWindowLoaded(object? sender, RoutedEventArgs args)
     {
-        try
+        RecalculateContentSize();
+    }
+
+    private void MainWindowResized(object? sender, WindowResizedEventArgs args)
+    {
+        RecalculateContentSize();
+    }
+
+    private void ContentListDoubleTapped(object? sender, TappedEventArgs args)
+    {
+        if (DataContext is MainWindowViewModel vm)
         {
-            DirectoryInfo directory = new(currentPath);
-            DirectoryInfo[] subdirectories = directory.GetDirectories();
-            FileInfo[] files = directory.GetFiles();
-            string[] content = new string[subdirectories.Length + files.Length];
-
-            for (int i = 0; i < subdirectories.Length; i++)
-            {
-                content[i] = subdirectories[i].Name;
-            }
-
-            for (int i = subdirectories.Length; i < subdirectories.Length + files.Length; i++)
-            {
-                content[i] = files[i - subdirectories.Length].Name;
-            }
-
-            contentList.ItemsSource = content;
-            model.FilesStartIndex = subdirectories.Length;
-
-            return true;
+            vm.OpenFolderOrLaunchFile();
         }
-        catch (DirectoryNotFoundException)
-        {
-            IMsBox<ButtonResult> messageBox = MessageBoxManager.GetMessageBoxStandard("Could Not Find Folder", "The selected folder could not be found.", ButtonEnum.Ok, MsBox.Avalonia.Enums.Icon.Error);
-            messageBox.ShowAsync();
+    }
 
-            return false;
-        }
-        catch (UnauthorizedAccessException)
+    private void PathTextFieldKeyDown(object? sender, KeyEventArgs args)
+    {
+        if (DataContext is MainWindowViewModel vm)
         {
-            IMsBox<ButtonResult> messageBox = MessageBoxManager.GetMessageBoxStandard("Permission Denied", "You do not have permission to open this folder.", ButtonEnum.Ok, MsBox.Avalonia.Enums.Icon.Error);
-            messageBox.ShowAsync();
-
-            return false;
+            vm.LoadPathFromPathTextField(args);
         }
     }
 
@@ -78,107 +50,5 @@ public partial class MainWindow : Window
 
         pathTextField.Width = Width - totalToolbarButtonsLength;
         contentList.Height = Height - toolbar.Bounds.Height;
-    }
-
-    private void MainWindowLoadedHandler(object? sender, RoutedEventArgs args)
-    {
-        RecalculateContentSize();
-    }
-
-    private void MainWindowResizedHandler(object? sender, WindowResizedEventArgs args)
-    {
-        RecalculateContentSize();
-    }
-
-    private void ContentListDoubleTappedHandler(object? sender, TappedEventArgs args)
-    {
-        string slash = OperatingSystem.IsWindows() ? "\\" : "/";
-
-        if (contentList.SelectedIndex < model.FilesStartIndex)
-        {
-            string newCurrentPath = model.CurrentPath + slash + contentList.SelectedItem;
-
-            if (DisplayContent(newCurrentPath))
-            {
-                model.PreviousPaths.Push(model.CurrentPath);
-
-                if (model.NextPaths.Count != 0)
-                {
-                    model.NextPaths.Clear();
-                }
-
-                model.CurrentPath = newCurrentPath;
-                pathTextField.Text = model.CurrentPath;
-                backButton.IsEnabled = model.PreviousPaths.Count > 0;
-                forwardButton.IsEnabled = model.NextPaths.Count > 0;
-            }
-        }
-        else
-        {
-            Process process = new();
-            ProcessStartInfo processStartInfo = new()
-            {
-                FileName = model.CurrentPath + slash + contentList.SelectedItem,
-                UseShellExecute = true
-            };
-            process.StartInfo = processStartInfo;
-
-            process.Start();
-        }
-    }
-
-    private void BackButtonClickHandler(object? sender, RoutedEventArgs args)
-    {
-        if (model.PreviousPaths.Count > 0)
-        {
-            model.NextPaths.Push(model.CurrentPath);
-
-            model.CurrentPath = model.PreviousPaths.Pop();
-            pathTextField.Text = model.CurrentPath;
-
-            DisplayContent(model.CurrentPath);
-        }
-
-        backButton.IsEnabled = model.PreviousPaths.Count > 0;
-        forwardButton.IsEnabled = model.NextPaths.Count > 0;
-    }
-
-    private void ForwardButtonClickHandler(object? sender, RoutedEventArgs args)
-    {
-        if (model.NextPaths.Count > 0)
-        {
-            model.PreviousPaths.Push(model.CurrentPath);
-
-            model.CurrentPath = model.NextPaths.Pop();
-            pathTextField.Text = model.CurrentPath;
-
-            DisplayContent(model.CurrentPath);
-        }
-
-        backButton.IsEnabled = model.PreviousPaths.Count > 0;
-        forwardButton.IsEnabled = model.NextPaths.Count > 0;
-    }
-
-    private void ReloadButtonClickHandler(object? sender, RoutedEventArgs args)
-    {
-        DisplayContent(model.CurrentPath);
-    }
-
-    private void PathTextFieldKeyDownHandler(object? sender, KeyEventArgs args)
-    {
-        if (args.Key == Key.Enter && pathTextField.Text != null && pathTextField.Text.Length > 0 && DisplayContent(pathTextField.Text))
-        {
-            model.PreviousPaths.Push(model.CurrentPath);
-
-            if (model.NextPaths.Count != 0)
-            {
-                model.NextPaths.Clear();
-            }
-
-            model.CurrentPath = pathTextField.Text;
-        }
-
-        backButton.IsEnabled = model.PreviousPaths.Count > 0;
-        forwardButton.IsEnabled = model.NextPaths.Count > 0;
     }
 }
