c462b099fbc58c68cabbd64e0185eae171ca82a3
DamareonC <markeec2002@gmail.com>
2025-11-22 15:25:22 -0600

    Navigate to parent folder

--- src/App.c
+++ src/App.c
@@ -1,9 +1,16 @@
+#include <glib.h>
 #include <gtk/gtk.h>
+
+#include <stdio.h>
 
 #include "App.h"
 
+char current_dir[1024];
+
 void run(GtkApplication* const app, const gpointer user_data)
 {
+    sprintf(current_dir, "%s", g_get_home_dir());
+
     GtkBuilder* const builder = gtk_builder_new();
     gtk_builder_add_from_file(builder, "res/main.ui", NULL);
 
@@ -11,8 +18,11 @@ void run(GtkApplication* const app, const gpointer user_data)
     gtk_window_set_application(GTK_WINDOW(window), app);
 
     const GObject* const list_box = gtk_builder_get_object(builder, "entry_list");
-    load_dir_entries(GTK_WIDGET(list_box), g_get_home_dir());
+    load_dir_entries(GTK_WIDGET(list_box), current_dir);
     load_css();
+
+    const GObject* const up_button = gtk_builder_get_object(builder, "up_button");
+    g_signal_connect(GTK_WIDGET(up_button), "clicked", G_CALLBACK(load_entry_dir), (gpointer)list_box);
 
     gtk_widget_set_visible(GTK_WIDGET(window), true);
     g_object_unref(builder);
--- src/App.h
+++ src/App.h
@@ -1,21 +1,52 @@
 #include <glib.h>
 #include <gtk/gtk.h>
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#define PATH_MAX_SIZE 1024
+
+extern char current_dir[PATH_MAX_SIZE];
 
 static void load_dir_entries(const GtkWidget* const list_box, const char* const path);
 
 static void load_entry_dir(GtkWidget* const widget, const gpointer data)
 {
-    const GtkWidget* const list_box = gtk_widget_get_parent(widget);
-    GtkListBoxRow* const list_box_row = GTK_LIST_BOX_ROW(widget);
-    GtkLabel* const label = GTK_LABEL(gtk_list_box_row_get_child(list_box_row));
-    const char* const entry_name = gtk_label_get_text(label);
-    static char new_dir[1024];
+    if (GTK_IS_LIST_BOX_ROW(widget))
+    {
+        const GtkWidget* const list_box = gtk_widget_get_parent(widget);
+        GtkListBoxRow* const list_box_row = GTK_LIST_BOX_ROW(widget);
+        GtkLabel* const label = GTK_LABEL(gtk_list_box_row_get_child(list_box_row));
+        const char* const entry_name = gtk_label_get_text(label);
+        const bool root = !strcmp(current_dir, "/");
 
-    sprintf(new_dir, "%s/%s", (const char*)data, entry_name);
-    load_dir_entries(list_box, new_dir);
+        sprintf(current_dir, root ? "%s%s" : "%s/%s", current_dir, entry_name);
+        load_dir_entries(list_box, current_dir);
+    }
+    else if (GTK_IS_BUTTON(widget))
+    {
+        if (!strcmp(current_dir, "/")) return;
+
+        const char* const last_slash = strrchr(current_dir, '/');
+        const char* c = current_dir;
+        const bool root_is_parent_dir = strchr(current_dir, '/') == last_slash;
+
+        for (int i = 0; i < PATH_MAX_SIZE; i++)
+        {
+            if (root_is_parent_dir)
+            {
+                memset(current_dir + 1, 0, PATH_MAX_SIZE - 1);
+                break;
+            }
+            else
+            {
+                if (++c > last_slash) current_dir[i] = 0;
+            }
+        }
+
+        load_dir_entries((GtkWidget*)data, current_dir);
+    }
 }
 
 static void load_dir_entries(const GtkWidget* const list_box, const char* const path)
@@ -34,7 +65,7 @@ static void load_dir_entries(const GtkWidget* const list_box, const char* const 
 
             GtkWidget* const list_box_row = gtk_list_box_row_new();
             gtk_list_box_append(GTK_LIST_BOX(list_box), list_box_row);
-            g_signal_connect(list_box_row, "activate", G_CALLBACK(load_entry_dir), (gpointer)path);
+            g_signal_connect(list_box_row, "activate", G_CALLBACK(load_entry_dir), NULL);
 
             GtkWidget* const label = gtk_label_new(dir_ent_ptr->d_name);
             gtk_list_box_row_set_child(GTK_LIST_BOX_ROW(list_box_row), label);
