6a165f2f10de9fd1c77f9ea28ea3f08c5a65e37e
DamareonC <markeec2002@gmail.com>
2025-11-26 02:47:12 -0600

    Added refresh button

--- docs/TODO.md
+++ docs/TODO.md
@@ -1,11 +1,11 @@
 Now:
 * Display all the entries in a path &check;
     * Order all entries (first all the folders, then the files; all in alphanumeric order) &check;
-    * For now, display hidden entries as well; later, it will be a toggle later
+    * For now, display hidden entries as well; later, it will be a toggle
     * Add navigation between paths &check;
 * Add a search for paths &check;
     * Make sure the path is valid (e.g. no invalid characters, the path exists, etc)
-* Add buttons for forward (next folder), back (previous folder), up (parent folder) &check;, home (home folder) &check;, and refresh (same folder)
+* Add buttons for forward (next folder), back (previous folder), up (parent folder) &check;, home (home folder) &check;, and refresh (same folder) &check;
     * The forward/back buttons should be disabled if there are no next/previous folders
     * The up button should be disabled if the current path is the root folder
     * The home button should be disabled if the current path is the home folder
--- src/Button.c
+++ src/Button.c
@@ -13,6 +13,11 @@ static void s_home_button_clicked(const GtkButton* const button, GlobalState* co
     set_global_state(data, g_get_home_dir());
 }
 
+static void s_refresh_button_clicked(const GtkButton* const button, GlobalState* const data)
+{
+    load_directory(data, ((GlobalState*)data)->current_path);
+}
+
 static void s_path_entry_activate(GtkEntry* const entry, GlobalState* const data)
 {
     GtkEntryBuffer* const entry_buffer = gtk_entry_get_buffer(entry);
@@ -43,6 +48,9 @@ void load_buttons(GtkBuilder* const builder, GlobalState* const global_state)
     
     GtkButton* const home_button = GTK_BUTTON(gtk_builder_get_object(builder, "home_button"));
     g_signal_connect(home_button, "clicked", G_CALLBACK(s_home_button_clicked), global_state);
+
+    GtkButton* const refresh_button = GTK_BUTTON(gtk_builder_get_object(builder, "refresh_button"));
+    g_signal_connect(refresh_button, "clicked", G_CALLBACK(s_refresh_button_clicked), global_state);
 
     GtkEntry* const path_entry = GTK_ENTRY(gtk_builder_get_object(builder, "path_entry"));
     g_signal_connect(path_entry, "activate", G_CALLBACK(s_path_entry_activate), global_state);
